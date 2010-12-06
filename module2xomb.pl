a24f81ca59f6f728a86652960c3f72185604b07b
wolfwood <transmethyl@gmail.com>
2010-12-06 03:12:48 -0500

    a script to turn kernel modules into object files that can be statically linked (without symbol collisions)

---
+++ module2xomb.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+use strict;
+my($o, $mod, $prefix);
+
+
+foreach $mod (@ARGV) {
+		$o = $mod;
+		$o =~ s/ko$/o/;
+		$prefix = $mod;
+		$prefix =~ s/.ko//;
+		
+		system("objcopy --redefine-sym init_module=init_$prefix\_module $mod $o");
+}
